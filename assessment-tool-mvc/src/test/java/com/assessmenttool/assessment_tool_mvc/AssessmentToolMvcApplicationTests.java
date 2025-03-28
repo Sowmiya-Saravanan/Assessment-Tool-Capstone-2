a2ee5a4bdaefcec197a7c450f355c1382cfcb285
Sowmiya <ssowmiyasaravananr@gmail.com>
2025-03-28 17:59:23 +0530

    Spring MVC

---
+++ assessment-tool-mvc/src/test/java/com/assessmenttool/assessment_tool_mvc/AssessmentToolMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessmenttool.assessment_tool_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentToolMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
